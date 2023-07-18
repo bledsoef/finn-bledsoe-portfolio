a23cbd36c4293a429a6d982ac6a2799348fb3c61
bledsoef <bledsoef@berea.edu>
2023-07-17 23:07:04 -0400

    now receive emails when someone asks a question

--- chatbot/main.py
+++ chatbot/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI, Request
 import uvicorn 
 from ai import AI
 from fastapi.middleware.cors import CORSMiddleware  # Import the CORS middleware
-
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+import ssl
 
 app = FastAPI()
 
@@ -18,6 +22,29 @@ app.add_middleware(
 async def index(request: Request):
    data = await request.json()  # Correct way to access the JSON data
    question = data.get("message")  # Assuming you are sending the message in the "message" field
+   try:
+      smtp_server = "smtp.gmail.com"
+      port = 587  # For starttls
+      SUBJECT = "Someone asked you a question!"
+      email = os.environ["SENDER_EMAIL"]
+      msg = MIMEMultipart()
+
+      password = os.environ["EMAIL_PASSWORD"]
+      msg['From'] = email
+      msg['To'] = email  # Replace with the recipient's email address
+      msg['Subject'] = SUBJECT
+      server = smtplib.SMTP(smtp_server, port)
+      msg.attach(MIMEText(question, 'plain'))
+      server.starttls()
+      server.login(email, password)
+      server.sendmail(email, email, msg.as_string())
+
+   except Exception as e:
+      print("Error:", e)
+
+   finally:
+      server.quit()
+
    chatHistory = parseChatHistory(data.get("chatHistory"))
    responseModel = AI(chat_history=chatHistory)
    response = responseModel.respond(question)
